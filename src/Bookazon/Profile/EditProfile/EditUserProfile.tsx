0e154252b6488dc4971bbc48cba849d49b303ffe
leo.senai <LSenai@gmail.com>
2024-04-13 21:20:03 -0400

    added cancel button

--- src/Bookazon/Profile/EditProfile/EditUserProfile.tsx
+++ src/Bookazon/Profile/EditProfile/EditUserProfile.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Button, TextField, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
+import SearchBar from "../../Home/SearchBar";
+import ProfileNav from "../../Home/ProfileNav"
+
 function EditUserProfile({ user }: { user: any }) {
     const navigate = useNavigate();
 
@@ -36,59 +39,70 @@ function EditUserProfile({ user }: { user: any }) {
     };
 
     return (
-        <Box sx={{ mx: '25rem', mt: '1rem', border: 1, borderColor: 'grey.500', p: 2 }}>
-            <h2>Edit Profile</h2>
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    label="Username"
-                    name="username"
-                    value={userData.username}
-                    onChange={handleChange}
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    label="Email"
-                    name="email"
-                    value={userData.email}
-                    onChange={handleChange}
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    label="Password"
-                    name="password"
-                    value={userData.password}
-                    onChange={handleChange}
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"
-                    type="password"
-                />
-                <TextField
-                    label="First Name"
-                    name="firstName"
-                    value={userData.firstName}
-                    onChange={handleChange}
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    label="Last Name"
-                    name="lastName"
-                    value={userData.lastName}
-                    onChange={handleChange}
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"
-                />
-                <Button type="submit" variant="contained" color="primary">Update</Button>
-                <Button variant="contained" color="error" onClick={handleCancel} sx={{mx: 2}}>Cancel</Button>
-            </form>
-        </Box>
+        <div>
+            <ProfileNav />
+            <SearchBar />
+            <Box sx={{ mx: '25rem', mt: '1rem', border: 1, borderColor: 'grey.500', p: 2 }}>
+                <h2>Edit Profile</h2>
+                <form onSubmit={handleSubmit}>
+                    <TextField
+                        label="Username"
+                        name="username"
+                        value={userData.username}
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <TextField
+                        label="Email"
+                        name="email"
+                        value={userData.email}
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <TextField
+                        label="Password"
+                        name="password"
+                        value={userData.password}
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                        variant="outlined"
+                        type="password"
+                    />
+                    <TextField
+                        label="First Name"
+                        name="firstName"
+                        value={userData.firstName}
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <TextField
+                        label="Last Name"
+                        name="lastName"
+                        value={userData.lastName}
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    {/* Additional Box for button alignment */}
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
+                        <Button type="submit" variant="contained" color="primary">
+                            Update
+                        </Button>
+                        <Button variant="contained" color="error" onClick={handleCancel} sx={{ ml: 2 }}>
+                            Cancel
+                        </Button>
+                    </Box>
+                </form>
+            </Box>
+        </div>
     );
 }
 
