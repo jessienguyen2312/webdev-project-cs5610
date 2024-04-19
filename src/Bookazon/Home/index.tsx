747d2bfcc5b58b5a47db146fe150889b22ac5959
Ghalia-AbuNowar <133063833+Ghalia-AbuNowar@users.noreply.github.com>
2024-04-19 10:50:26 -0400

    some extre fetures

--- src/Bookazon/Home/index.tsx
+++ src/Bookazon/Home/index.tsx
@@ -3,19 +3,25 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import BookShelf from "./BookShelf";
 import ListOfBooks from "./ListOfBooks";
-import Profile from "./Profile";
+import ProfileAnon from "./ProfileAnon";
 import TitleBar from "./TitleBar";
+import ProfileUser from "./ProfileUser";
 
 
 function Home() {
+    // to navigate between an anon and non anon user test
+    const user = null
+     const userRole = 'admin'
     return (
+
         <div>
             {/* <h1>Bookazon :D</h1> */}
             <TitleBar />
-            <Profile />
+            {/* needs to be changed to incorporate the actual session variables */}
+            {user === null ? (<ProfileAnon />): (<ProfileUser userName = {user}/>)}
+            
             <SearchBar />
             <ListOfBooks />
-            {/* <BookShelf genre={"romance"} /> */}
         </div>
     )
 }
