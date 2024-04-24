18b4caa450949de4ce8f1620fd28515b8b4664e0
leo.senai <LSenai@gmail.com>
2024-04-24 01:50:22 -0400

    render views based on private/public -- need to finish profile editing features.

--- src/Bookazon/Profile/FavoriteBooks.tsx
+++ src/Bookazon/Profile/FavoriteBooks.tsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
+import { bookDetailBookey } from '../clientExternal'; 
+import {Link, useNavigate} from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { setBook } from '../BookDetail/BookReducer';
 
-function FavoriteBooks({ books }: { books: any[] }) {
+
+interface BookDetail {
+    id: string;
+    title: string;
+    author_name: string[];
+    cover: string;
+    work_key: string;
+}
+
+interface FavoriteBooksProps {
+    bookIds: string[]; // Adjusting the prop type here
+}
+
+function FavoriteBooks({ bookIds }: FavoriteBooksProps) {
+    const [books, setBooks] = useState<BookDetail[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const bookPromises = bookIds.map(bookId => bookDetailBookey(bookId));
+            const bookDetails = await Promise.all(bookPromises);
+            console.log("Book details fetched:", bookDetails); // Log to see what the book IDs look like
+            const formattedBooks = bookDetails.map(book => ({
+                id: book.key.replace('/books/', ''), // Strip the '/books/' prefix
+                title: book.title,
+                author_name: book.author_name,
+                cover: book.cover_edition_key,
+                work_key: book.key
+            }));
+            setBooks(formattedBooks);
+            setLoading(false);
+        };
+
+        fetchData();
+    }, [bookIds]);
+
+    const bookDetailPage = (bookItem: BookDetail) => {
+        dispatch(setBook({
+            key: bookItem.id,
+            author_name: bookItem.author_name,
+            cover: bookItem.cover,
+            work_key: bookItem.work_key
+        }));
+        navigate(`/Bookazon/BookDetail/${bookItem.id}`);
+    };
+
+    if (loading) return <div>Loading favorite books...</div>;
+
     return (
-        <Box sx={{ mx: '25rem', mt: '1rem', border: 1, borderColor: 'grey.500', p: 2 }}>
+        <Box sx={{ mt: '1rem', border: 1, borderColor: 'grey.500', p: 2 }}>
             <h3>Favorite Books</h3>
             <ul>
                 {books.map(book => (
-                    <li key={book.key}>{book.key} - Placeholder for book title</li>
+                    <li key={book.id} onClick={() => bookDetailPage(book)} >
+                        {book.title}
+                    </li>
                 ))}
             </ul>
         </Box>
